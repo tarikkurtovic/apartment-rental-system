ec2ad6bd3428c2d6e4988e5c353d9c09070073f4
tarikkurtovic <tarik.kurtovic@stu.ibu.edu.ba>
2025-12-28 16:36:13 +0100

    fixed

--- backend/config.php
+++ backend/config.php
@@ -1,8 +1,8 @@
 <?php
 
 // Set the reporting
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
 
 class Config{
