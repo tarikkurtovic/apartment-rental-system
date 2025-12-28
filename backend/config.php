9ee8c3948f2a5fa97a0ebebb0420e417d1a740a9
tarikkurtovic <tarik.kurtovic@stu.ibu.edu.ba>
2025-12-28 01:52:19 +0100

    deployment

--- backend/config.php
+++ backend/config.php
@@ -5,8 +5,34 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
 
+class Config{
+    public static function DB_NAME() {
+       return Config::get_env("DB_NAME", "montana");
+   }
+   public static function DB_PORT() {
+       return Config::get_env("DB_PORT", 3306);
+   }
+   public static function DB_USER() {
+       return Config::get_env("DB_USER", 'root');
+   }
+   public static function DB_PASSWORD() {
+       return Config::get_env("DB_PASSWORD", '');
+   }
+   public static function DB_HOST() {
+       return Config::get_env("DB_HOST", '127.0.0.1');
+   }
+   public static function JWT_SECRET() {
+       return Config::get_env("JWT_SECRET", '627e0ad9a18a0d996dd1be1a145fefe3');
+}
+   public static function get_env($name, $default){
+       return isset($_ENV[$name]) && trim($_ENV[$name]) != "" ? $_ENV[$name] : $default;
+   }
+}
 
-class Config
+
+
+
+/*class Config
 {
    public static function DB_NAME()
    {
@@ -34,3 +60,4 @@ class Config
        return '627e0ad9a18a0d996dd1be1a145fefe3';
    }
 }
+*/
